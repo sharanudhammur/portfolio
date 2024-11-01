c39d7ce74529af2edaceed9aba312bd3d9560225
sharanu dhammur <sharandhammur@gmail.com>
2024-11-01 21:05:51 +0530

    major changes

--- src/App.js
+++ src/App.js
@@ -7,22 +7,51 @@ import Project from './Screens/Project/index';
 import Footer from './Screens/Footer/index';
 import Header from './Screens/Header/index';
 import About from './Screens/About/index';
+import { useRef } from 'react';
 // import Techn from './Screens/Techn/Techn';
 // import Tech2 from './Screens/Tech2';
 
 function App() {
 
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const experienceRef = useRef(null);
+  const techRef = useRef(null);
+  const projectRef = useRef(null);
+  const footerRef = useRef(null);
+
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+};
+
   return (
     <div className="App">
-      <Header/>
-      <Home/>
-      <About/>
-      <Experience/>
-      <Tech/>
-      {/* <Tech2/> */}
-      {/* <Techn/> */}
-      <Project/>
-      <Footer/>
+   <Header 
+                scrollToHome={() => scrollToSection(homeRef)}
+                scrollToAbout={() => scrollToSection(aboutRef)}
+                scrollToExperience={() => scrollToSection(experienceRef)}
+                scrollToTech={() => scrollToSection(techRef)}
+                scrollToProject={() => scrollToSection(projectRef)}
+                scrollToFooter={() => scrollToSection(footerRef)}
+            />
+            <div ref={homeRef}>
+                <Home />
+            </div>
+            <div ref={aboutRef}>
+                <About />
+            </div>
+            <div ref={experienceRef}>
+                <Experience />
+            </div>
+            <div ref={techRef}>
+                <Tech />
+            </div>
+            <div ref={projectRef}>
+                <Project />
+            </div>
+            <div ref={footerRef}>
+                <Footer />
+            </div>
     </div>
   );
 }
--- src/Screens/Home/index.jsx
+++ src/Screens/Home/index.jsx
@@ -1,72 +1,47 @@
-import React from 'react'
-import "./styles.scss"
+import React, { useEffect, useState } from 'react';
+import "./styles.scss";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
-// import { ReactComponent as DotImage } from "../../Assets/Images/DotImage.svg";
-// import {component}
-// import { UnFilledHeartIcon, FilledHeartIcon, CoffeeWebLogo, devEnvLogo } from 'Assets/Icons'
+import ProfilePic from "../../Assets/Images/image3.jpeg";
 
 function Home() {
+    const [isVisible, setIsVisible] = useState(false);
 
     const links = [
-        {
-            linkName: "Instagram",
-            icon: "https://static-00.iconduck.com/assets.00/facebook-icon-512x512-seb542ju.png",
-            icon2: <FaInstagram />,
-            url: ""
-        },
-        {
-            linkName: "Github",
-            icon: "https://static-00.iconduck.com/assets.00/facebook-icon-512x512-seb542ju.png",
-            icon2: <FaGithub />,
-                url: ""
-        },
-        {
-            linkName: "LinkedIn",
-            icon: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
-            icon2: <FaLinkedin />,
-                url: "https://www.linkedin.com/in/sharanudhammur/"
-        },
-        {
-            linkName: "mail",
-            icon: "https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png",
-            icon2: <IoMdMail />,
-                url: "https://www.linkedin.com/in/sharanudhammur/"
-        },
-        {
-            linkName: "Twitter",
-            icon: "https://www.svgrepo.com/show/353196/mail-with-circle.svg",
-            icon2: <FaSquareXTwitter />,
-                url: ""
-        },
-    ]
+        { linkName: "Instagram", icon2: <FaInstagram />, url: "" },
+        { linkName: "Github", icon2: <FaGithub />, url: "" },
+        { linkName: "LinkedIn", icon2: <FaLinkedin />, url: "https://www.linkedin.com/in/sharanudhammur/" },
+        { linkName: "Mail", icon2: <IoMdMail />, url: "https://www.linkedin.com/in/sharanudhammur/" },
+        { linkName: "Twitter", icon2: <FaSquareXTwitter />, url: "" },
+    ];
+
+    useEffect(() => {
+        setIsVisible(true); // Trigger the animation when component mounts
+    }, []);
+
     return (
         <div className='home-screen'>
             <div className='home-screen-wrapper'>
                 <div className='aboutme'>
-                        <img className='immm' src="https://media.licdn.com/dms/image/v2/D5603AQGkuCjAMtroqA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1726069489970?e=1735776000&v=beta&t=HruD_-wTKiH1f4pyy4IVqS-8h3bAKBB1Ti8YInGH4FQ" alt="" />
+                    <img className={`immm ${isVisible ? 'slide-in' : ''}`} src={ProfilePic} alt="Profile" />
                     <div className='Hey_there'>Hey there!, I'm-</div>
                     <div className='firstname'>Sharan</div>
-                    {/* <div className='designation'>MERN stack Developer</div> */}
-                    <div className='about'><span style={{ color: "black" }}>Senior Software Engineer.</span> A self-taught developer with an
-                        interest in Computer Science.</div>
-                        {/* <button className='title' style={{backgroundColor:"#3CCF91", color:"white", width:"170px", border:"0", borderRadius:"4px", padding:"10px 0px", fontSize:"18px"}}>Resume</button> */}
-                    <div className='links'>
-                        {links.map((ele) => {
-                            return (
-                                <a className='link-icon' href={ele.url} target='_blank' rel="noopener noreferrer">
-                                    {/* <img src={ele.icon} alt="" /> */}
-                                    {ele.icon2}
-                                </a>
-                            )
-                        })}
+                    <div className='about'>
+                        <span style={{ color: "black" }}>Senior Software Engineer.</span> A self-taught developer with an interest in Computer Science.
                     </div>
+                    <div className='links'>
+                        {links.map((ele) => (
+                            <a key={ele.linkName} className='link-icon' href={ele.url} target='_blank' rel="noopener noreferrer">
+                                {ele.icon2}
+                            </a>
+                        ))}
+                    </div>
+                        <a target='_blank' href='https://drive.google.com/file/d/1X-n4hY0lyhgk739s55vJS4wyPFr7OvGz/view' rel="noopener noreferrer" className='resume-button'>Resume</a>
                 </div>
-                <div className='banner-image'><img src="https://dsbalico.netlify.app/photo.jpg" alt="" /></div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
