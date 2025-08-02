ecbba0d7f151267709d9d7457dc1d43a4017714e
sharanu dhammur <150852531+sharanudhammur@users.noreply.github.com>
2025-08-02 15:22:30 +0530

    Update index.jsx

--- src/Screens/About/index.jsx
+++ src/Screens/About/index.jsx
@@ -1,18 +1,84 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.scss";
 
 function About() {
   return (
-    <div className='about-screen'>
-      <div className='about-screen-wrapper'>
-        <div className='title'>About me</div>
+    <div className="about-screen">
+      <div className="about-screen-wrapper">
+        <div className="title">About me</div>
         <div>
-          <div className='about-me'>
+          <div className="about-me">
             <div>
-              Hello, I’m <strong className='highlight'>Sharan</strong>, a Senior Software Engineer at <strong className='highlight'>COFFEEWEB TECHNOLOGIES PVT. LTD.</strong>, with over <strong className='highlight'>3 years</strong> of experience in software development. I specialize in designing and developing scalable web applications, leveraging my expertise in both frontend and backend technologies.
+              <p>
+                Hi, my name is <span className="highlight">Sharanabasappa</span>
+                . I'm a
+                <span className="highlight"> MERN Full Stack Developer </span>{" "}
+                with over
+                <span className="highlight">4 years</span> of experience
+                building scalable, high-performance web applications.
+              </p>
+              <p>
+                I specialize in technologies like{" "}
+                <span className="highlight">React, Node.js, MongoDB,</span> and
+                <span className="highlight"> Express.js</span>, with hands-on
+                experience in
+                <span className="highlight">
+                  {" "}
+                  Next.js, TypeScript,
+                </span> and <span className="highlight">React Native</span>.
+              </p>
+              <p>
+                Currently, I’m working as a{" "}
+                <span className="highlight">Senior Software Engineer</span> at
+                <span className="highlight"> CoffeeWeb Technologies</span>,
+                where I contribute to a global trading web application providing
+                real-time coffee industry data including live prices, news,
+                insights, notifications, and much more
+              </p>
+              <span>Some key features I've built:</span>
+              <ul>
+                <li>Single-device sign-in</li>
+                <li>Google Authentication for secure login</li>
+                <li>Progressive Web App (PWA) support with Service Workers</li>
+                <li>Firebase Push Notifications</li>
+                <li>
+                  Lightweight candlestick and line charts for historical data
+                </li>
+                <li>
+                  Performance-focused enhancements (lazy loading, code
+                  splitting, etc.)
+                </li>
+              </ul>
             </div>
-            <div style={{ marginTop: '20px' }}>
-              Proficient in <strong className='highlight'>MongoDB, Express.js, React.js, and Node.js</strong>, I focus on building seamless, user-centric applications with a strong emphasis on <strong className='highlight'>RESTful API design</strong> and <strong className='highlight'>database architecture</strong>. I am passionate about creating innovative solutions that enhance user experiences and thrive in collaborative, cross-functional team environments.
+
+            <div style={{ marginTop: "20px" }}>
+              <span>Roles & Responsibilities</span>
+              <ul>
+                <li>
+                  Develop scalable, component-based, responsive UIs using React
+                  and modern CSS frameworks
+                </li>
+                <li>
+                  Optimize app performance using lazy loading, code splitting,
+                  and caching strategies
+                </li>
+                <li>
+                  Design and build RESTful APIs with Node.js and Express.js
+                </li>
+                <li>Manage authentication using JWT and OAuth</li>
+                <li>
+                  Implement backend middleware and error-handling for
+                  reliability
+                </li>
+                <li>
+                  Design MongoDB schemas and handle database operations with
+                  Mongoose
+                </li>
+                <li>
+                  Use Git & GitHub for version control and team collaboration
+                </li>
+                <li>Perform API testing using Postman and Swagger</li>
+              </ul>
             </div>
           </div>
         </div>
