254ded2fb0c7a29e30a33d894f830a732249ffb1
sharanu dhammur <150852531+sharanudhammur@users.noreply.github.com>
2026-02-04 08:50:04 +0530

    Delete src directory

--- src/Components/TitleComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./TitleComponent.scss";
-
-function TitleComponent({ children }) {
-  return <div className="title-component">{children}</div>;
-}
-
-export default TitleComponent;
